6da40f5d0bfa31fd695af02871655e53e450c8fa
Kajal Thombare <kajalthombare02@gmail.com>
2026-05-05 10:41:15 +0530

    fixed database for deployment

--- database.py
+++ database.py
@@ -1,1 +1,9 @@
-DATABASE_URL = "mysql+pymysql://root:1234@localhost/inventory"
+import os
+from sqlalchemy import create_engine
+
+DATABASE_URL = os.getenv("MYSQL_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
