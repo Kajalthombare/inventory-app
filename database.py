07085d269289ca6ccfc777dad1df378f717f2dd3
Kajal Thombare <kajalthombare02@gmail.com>
2026-05-05 11:59:38 +0530

    fix mysql driver

--- database.py
+++ database.py
@@ -4,9 +4,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("MYSQL_URL")
 
-if not DATABASE_URL:
-    raise ValueError("MYSQL_URL not set")
-
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
