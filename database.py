5088a66255dd0de2944cc69f70f0bc04fa2ea9d3
Kajal Thombare <kajalthombare02@gmail.com>
2026-05-05 11:37:41 +0530

    fix railway db connection

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("MYSQL_URL")   # 👈 THIS LINE IS CRITICAL
+DATABASE_URL = os.getenv("MYSQL_URL")
+
+if not DATABASE_URL:
+    raise ValueError("MYSQL_URL not set")
 
 engine = create_engine(
     DATABASE_URL,
