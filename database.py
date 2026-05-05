6f811b6c4550968af4d628cbf1a71ec1350a6b9f
Kajal Thombare <kajalthombare02@gmail.com>
2026-05-05 12:00:33 +0530

    fix mysql driver

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("MYSQL_URL")
+print("DB URL:", DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,
